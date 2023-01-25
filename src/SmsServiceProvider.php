d1f810cd157534e92d15115d2c33ebc5dbc87a89
Dinush Chathurya <dinushchathurya21@gmail.com>
2023-01-25 11:45:44 +0530

    :sparkles: Create SmsServiceProvider.php

---
+++ src/SmsServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dinushchathurya\Websmslk;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmsServiceProvider extends ServiceProvider
+{
+    
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/sms.php' => config_path('sms.php'),
+        ]);
+    }
+
+    public function register()
+    {
+        $this->app->bind('sms', function () {
+            return new Sms;
+        });
+    }
+
+}
