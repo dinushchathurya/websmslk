bad20bca68204619bed9d9ac7827b9db60eebaef
Dinush Chathurya <dinushchathurya21@gmail.com>
2023-02-01 19:52:46 +0530

    :hammer: Add scheduledate option

--- src/Jobs/SendSms.php
+++ src/Jobs/SendSms.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Carbon\Carbon;
 
 class SendSms implements ShouldQueue
 {
@@ -14,22 +15,32 @@ class SendSms implements ShouldQueue
 
     protected $numbers;
     protected $message;
+    protected $scheduledate;
 
-    public function __construct($numbers, $message)
+    public function __construct($numbers, $message, $scheduledate = null)
     {
-        $this->numbers = $numbers;
-        $this->message = $message;
+        $this->numbers      = $numbers;
+        $this->message      = $message;
+        $this->scheduledate = $scheduledate ?: Carbon::now()->toDateTimeString();
     }
 
     public function handle()
     {
-        $apiKey = config('sms.api_key');
-        $apiToken = config('sms.api_token');
-        $senderId = config('sms.sender_id');
-        $route = config('sms.route');
-        foreach ($this->numbers as $number) {
-            $url = "https://cloud.websms.lk/smsAPI?sendsms&apikey=" . $apiKey . "&apitoken=" . $apiToken . "&type=sms&from=" . $senderId . "&to=" . $number . "&text=" . urlencode($this->message) . "&route=" . $route;
-            file_get_contents($url);
+        try {
+            
+            $apiKey   = config('sms.api_key');
+            $apiToken = config('sms.api_token');
+            $senderId = config('sms.sender_id');
+            $type     = config('sms.type');
+            $route    = config('sms.route');
+
+            foreach ($this->numbers as $number) {
+                $url = "https://cloud.websms.lk/smsAPI?sendsms&apikey=" . $apiKey . "&apitoken=" . $apiToken . "&type=" .$type . "&from=" . $senderId . "&to=" . $number . "&text=" . urlencode($this->message) . "&scheduledate=" . urlencode($this->scheduledate) . "&route=" . $route;
+                file_get_contents($url); 
+            }
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
         }
     }
 }
