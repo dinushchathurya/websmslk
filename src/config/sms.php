cf49eaff1d153321847b4a5b9bf405bd91a4d952
Dinush Chathurya <dinushchathurya21@gmail.com>
2023-02-01 19:53:03 +0530

    :hammer: Add scheduledate option

--- src/config/sms.php
+++ src/config/sms.php
@@ -4,5 +4,6 @@ return [
     'api_key'   => env('SMS_API_KEY'),
     'api_token' => env('SMS_API_TOKEN'),
     'sender_id' => env('SMS_SENDER_ID'),
+    'type'      => env('SMS_TYPE', "sms"),
     'route'     => env('SMS_ROUTE', 0)
 ];
